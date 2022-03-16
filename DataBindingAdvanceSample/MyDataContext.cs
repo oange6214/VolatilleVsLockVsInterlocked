8bb17b07b3099a40962fdcb7b2a6fa9cc4502d67
柏翰 林 <his6214@hotmail.com>
2022-03-17 00:28:44 +0800

    DataBindingAdvanceSample

---
+++ DataBindingAdvanceSample/MyDataContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBindingAdvanceSample
+{
+    public class MyDataContext
+    {
+        public MyClass MyClass { get; set; } = new MyClass();
+    }
+
+    public class MyClass : INotifyPropertyChanged
+    {
+
+        private double _myValue;
+
+        public double MyValue
+        {
+            get { return _myValue; }
+            set { _myValue = value; OnPropertyChanged(); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
