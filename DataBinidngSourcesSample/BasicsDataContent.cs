fa60222aacbfee6b2f12c3e5cd73438580da6131
柏翰 林 <his6214@hotmail.com>
2022-03-17 00:04:39 +0800

    Add DataBinidngSourcesSample

---
+++ DataBinidngSourcesSample/BasicsDataContent.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace DataBinidngSourcesSample
+{
+    public class BasicsDataContent : INotifyPropertyChanged
+    {
+        // prop
+        public double Value { get; set; } = 0.5;
+
+        // propfull
+        private double _number = 1;
+
+        public double Number
+        {
+            get { return _number; }
+            set { _number = value; }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
